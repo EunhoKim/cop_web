65cdb993d05b4db5b4838fa8b89dbcf6e37f84bd
eunho <eh5846@hotmail.com>
2017-11-17 10:58:15 +0900

    코드 오류 수정

--- src/reducers/memo.js
+++ src/reducers/memo.js
@@ -30,7 +30,7 @@ export default function memo(state, action) {
 	console.log("reducers==========memo : " + state + " ,action :" + action.type );
     if(typeof state === "undefined") {
         state = initialState;
-    }`
+    }
 
     switch(action.type) {
         /* MEMO_POST */
