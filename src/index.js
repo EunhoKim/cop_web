9eaada716da276ebbef0bab5daa161e443c182de
eunho <eh5846@hotmail.com>
2017-11-21 13:56:12 +0900

    원복

--- src/index.js
+++ src/index.js
@@ -5,28 +5,16 @@ import { App, Home, Login, Register, Wall } from 'containers';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import { syncHistoryWithStore } from 'react-router-redux';
-
-
-import configureStore from './store/configureStore';
-
 import reducers from 'reducers';
 import thunk from 'redux-thunk';
 
-//require('expose?$!expose?jQuery!jquery');
-//require('bootstrap-webpack');
 
-injectTapEventPlugin();
-const store = configureStore();
-const history = syncHistoryWithStore(browserHistory, store);
-
-//const store = createStore(reducers, applyMiddleware(thunk));
+const store = createStore(reducers, applyMiddleware(thunk));
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
     <Provider store={store}>
-        <Router history={history}>
+        <Router history={browserHistory}>
             <Route path="/" component={App}>
                 <IndexRoute component={Home}/>
                 <Route path="home" component={Home}/>
